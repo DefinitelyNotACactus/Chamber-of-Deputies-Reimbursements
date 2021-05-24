cebaa87c4e1415195d1d7fd53f3d66b6250dd936
DefinitelyNotACactus <galvao2010@icloud.com>
2021-05-24 14:58:00 -0300

    Update Tarefa 4

--- concat_df.py
+++ concat_df.py
@@ -1,9 +1,12 @@
 from pandas import read_csv, concat
 
 # Retorna a base de dados original, a partir dos arquivos csvs disponiveis no repositorio do github
-def concat_df():
+def concat_df(processed=False):
     dfs = []
-    csvs = ['data/'+ str(x) + '.csv' for x in range(2009, 2018)] # List comprehension dos csvs
+    if processed is True:
+        csvs = ['data/'+ str(x) + '_tratado.csv' for x in range(2009, 2018)] # List comprehension dos csvs
+    else:
+        csvs = ['data/'+ str(x) + '.csv' for x in range(2009, 2018)] # List comprehension dos csvs
     for csv in csvs:
         df = read_csv(csv)
         dfs.append(df)
